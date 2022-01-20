f84cd6d5e049fe93c42cf05ab38a867816908dd5
DevExpressExampleBot <fakeEmail>
2022-01-20 16:08:47 +0300

    Source auto update [skip ci]

--- VB/Spreadsheet_Document_Api_Part_3/CodeUtils/SpreadsheetExampleEvaluatorByTimer.vb
+++ VB/Spreadsheet_Document_Api_Part_3/CodeUtils/SpreadsheetExampleEvaluatorByTimer.vb
@@ -1,18 +1,17 @@
-﻿Namespace Spreadsheet_Document_Api_Part_3
-	#Region "SpreadsheetExampleEvaluatorByTimer"
-	Public Class SpreadsheetExampleEvaluatorByTimer
-		Inherits ExampleEvaluatorByTimer
+Namespace Spreadsheet_Document_Api_Part_3
 
-		Public Sub New()
-			MyBase.New()
-		End Sub
+#Region "SpreadsheetExampleEvaluatorByTimer"
+    Public Class SpreadsheetExampleEvaluatorByTimer
+        Inherits ExampleEvaluatorByTimer
 
-		Protected Overrides Function GetExampleCodeEvaluator(ByVal language As ExampleLanguage) As ExampleCodeEvaluator
-			If language = ExampleLanguage.VB Then
-				Return New SpreadsheetVbExampleCodeEvaluator()
-			End If
-			Return New SpreadsheetCSExampleCodeEvaluator()
-		End Function
-	End Class
-	#End Region
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Protected Overrides Function GetExampleCodeEvaluator(ByVal language As ExampleLanguage) As ExampleCodeEvaluator
+            If language = ExampleLanguage.VB Then Return New SpreadsheetVbExampleCodeEvaluator()
+            Return New SpreadsheetCSExampleCodeEvaluator()
+        End Function
+    End Class
+#End Region
 End Namespace
